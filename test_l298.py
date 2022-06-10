7a3493821241dcca4137ad2869a30d5c58357990
sonchung67365 <sonchung67365@gmail.com>
2022-06-10 12:10:03 +0700

    "vdc"

---
+++ test_l298.py
@@ -0,0 +1,78 @@
+import RPi.GPIO as GPIO
+import time
+
+class Motor():
+    def __init__(self, in1, in2):
+        self.in1 = in1
+        self.in2 = in2
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        self.in1_pwm = GPIO.PWM(self.in1, 100)
+        self.in2_pwm = GPIO.PWM(self.in2, 100)
+        self.in1_pwm.start(0)
+        self.in2_pwm.start(0)
+    def moveF(self, speed=50, t=0):
+        self.in1_pwm.ChangeDutyCycle(0)
+        self.in2_pwm.ChangeDutyCycle(speed)
+        time.sleep(t)
+    def moveB(self, speed=50, t=0):
+        self.in1_pwm.ChangeDutyCycle(speed)
+        self.in2_pwm.ChangeDutyCycle(0)
+        time.sleep(t)
+    def stop(self, t=0):
+        self.in1_pwm.ChangeDutyCycle(0)
+        self.in2_pwm.ChangeDutyCycle(0)
+        time.sleep(t)
+
+class MotorRobot():
+    def __init__(self, in1, in2, in3, in4):
+        self.in1 = in1
+        self.in2 = in2
+        self.in3 = in3
+        self.in4 = in4
+
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.in3, GPIO.OUT)
+        GPIO.setup(self.in4, GPIO.OUT)
+
+        self.in1 = GPIO.PWM(self.in1, 100)
+        self.in2 = GPIO.PWM(self.in2, 100)
+        self.in3 = GPIO.PWM(self.in3, 100)
+        self.in4 = GPIO.PWM(self.in4, 100)
+
+        self.in1.start(0)
+        self.in2.start(0)
+        self.in3.start(0)
+        self.in4.start(0)
+    
+    def move(self, speed=50, turn=0, t=0):
+        leftSpeed = speed - turn
+        rightSpeed = speed + turn
+
+        if leftSpeed > 100 : leftSpeed = 100
+        elif leftSpeed < -100 : leftSpeed = -100
+        if rightSpeed > 100 : rightSpeed = 100
+        elif rightSpeed < -100 : rightSpeed = -100
+
+        if rightSpeed > 0 :
+            self.in1.ChangeDutyCycle(0)
+            self.in2.ChangeDutyCycle(rightSpeed)
+        else:
+            self.in1.ChangeDutyCycle(abs(rightSpeed))
+            self.in2.ChangeDutyCycle(0)
+        if leftSpeed > 0 :
+            self.in3.ChangeDutyCycle(0)
+            self.in4.ChangeDutyCycle(leftSpeed)
+        else:
+            self.in3.ChangeDutyCycle(abs(leftSpeed))
+            self.in4.ChangeDutyCycle(0)
+
+        time.sleep(t)
+    
+    def stop(self, t=0):
+        self.in1.ChangeDutyCycle(0)
+        self.in2.ChangeDutyCycle(0)
+        self.in3.ChangeDutyCycle(0)
+        self.in4.ChangeDutyCycle(0)
+        time.sleep(t)
--- web_server.py
+++ web_server.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, make_response
 from datetime import timedelta, datetime
 from time import sleep
+import test_l298 as L298
+import RPi.GPIO as GPIO
+
+IN_1 = 17
+IN_2 = 27
+IN_3 = 22
+IN_4 = 23
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "batchedobaylen"
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+motor = L298.MotorRobot(IN_1, IN_2, IN_3, IN_4)
+
  
 @app.route('/')
 def welcome():
@@ -65,6 +78,8 @@ def control_motor():
     speed = request.form['speed']
     # Print value
     print(int(speed))
+    motor.move(speed=int(speed), t=0)
+
 
     # Get direction value
     dir = request.form['dir']
@@ -76,8 +91,29 @@ def control_motor():
 @app.route('/stop-motor')
 def stop_motor():
     print("Stop Motor")
+    motor.stop()
     return render_template('user.html')
+
+@app.route('/<changepin>', methods=['POST'])
+def reroute(changepin):
+    changePin = int(changepin) #cast changepin to an int
+    if changePin == 1:
+        print ("Left")
+        
+    elif changePin == 2:
+        print ("Forward")
+        
+    elif changePin == 3:
+        print ("Right")
+        
+    elif changePin == 4:
+        print ("Reverse")
+        
+    else:
+        pass
+    response = make_response(redirect(url_for('index')))
+    return(response)
 
 # Run the app on the local development server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="192.168.1.170", port="2000" ,debug=True)
